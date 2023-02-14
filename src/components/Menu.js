2dd68a45d0b85c414df71a770e5d5da1b5ab3776
Higor Rodrigues <higor_1992@hotmail.com>
2023-02-14 15:47:04 -0300

    logo removido

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,46 +1,44 @@
-import React from 'react'
+import React from "react";
 
-import caminho from "../img/Caminho.png"
-import config from "../img/config.png"
-import conquist from "../img/conquist.png"
-import dashboard from "../img/dashboard.png"
-import help from "../img/help.png"
-import voltbras from "../img/Voltbras.png"
-import retangulo from "../img/retangulo.png"
-
+import caminho from "../img/Caminho.png";
+import config from "../img/config.png";
+import conquist from "../img/conquist.png";
+import dashboard from "../img/dashboard.png";
+import help from "../img/help.png";
+import voltbras from "../img/Voltbras.png";
+import retangulo from "../img/retangulo.png";
 
 export const Menu = () => {
-    return (
-        <div>
-            <nav className="navbar">
-                <a className="navbar-item">
-                    <img src={voltbras} className="voltbras" />
-                    <span className='voltbrasSpan'>Voltbras App</span>
-                </a>
-                <a className="navbar-item active">
-                    <div>
-                        <img src={retangulo} className="fundo" />
-                        <img src={caminho} className="lista" />
-                        <span className='listaSpan'>Lista</span>
-                    </div>
-                </a>
-                <a className="navbar-item">
-                    <img src={conquist} />
-                    <span>Conquistas</span>
-                </a>
-                <a className="navbar-item">
-                    <img src={dashboard} />
-                    <span>Pokédex</span>
-                </a>
-                <a className="navbar-item">
-                    <img src={help} />
-                    <span>Ajuda</span>
-                </a>
-                <a className="navbar-item">
-                    <img src={config} />
-                    <span>Configuração</span>
-                </a>
-            </nav>
-        </div>
-    )
-}
+  return (
+    <div>
+      <nav className="navbar">
+        <a className="navbar-item">
+          <span className="voltbrasSpan"></span>
+        </a>
+        <a className="navbar-item active">
+          <div>
+            <img src={retangulo} className="fundo" />
+            <img src={caminho} className="lista" />
+            <span className="listaSpan">Lista</span>
+          </div>
+        </a>
+        <a className="navbar-item">
+          <img src={conquist} />
+          <span>Conquistas</span>
+        </a>
+        <a className="navbar-item">
+          <img src={dashboard} />
+          <span>Pokédex</span>
+        </a>
+        <a className="navbar-item">
+          <img src={help} />
+          <span>Ajuda</span>
+        </a>
+        <a className="navbar-item">
+          <img src={config} />
+          <span>Configuração</span>
+        </a>
+      </nav>
+    </div>
+  );
+};
